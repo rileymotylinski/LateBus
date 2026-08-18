b65766341ded6ee23d0624b6a8badc1cbde0f97a
rileymotylinski <riley@motylinski.com>
2026-08-18 11:16:19 -0500

    feat: init gtfs

--- gtfs.py
+++ gtfs.py
@@ -7,4 +7,6 @@ response = requests.get('https://svc.metrotransit.org/mtgtfs/tripupdates.pb')
 feed.ParseFromString(response.content)
 for entity in feed.entity:
  
-    print(entity)
+    print((entity.trip_update.trip.trip_id, entity.trip_update.trip.route_id))
+    for delay in entity.trip_update.stop_time_update:
+        print(delay)
